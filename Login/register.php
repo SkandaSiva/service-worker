4692ebeb4f97cf760d994c695761aaff44bd745f
mibra004 <77853813+mibra004@users.noreply.github.com>
2024-02-05 21:15:38 -0500

    login-2

--- Login/register.php
+++ Login/register.php
@@ -1,4 +1,24 @@
 <?php
 
+if (empty($_POST['name'])) {
+    die("Name is required")
+
+}
+
+if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required");
+}
+
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
+}
+
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+    die("Password must contain at least one number");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+var_dump($password_hash);
 print_r($_POST);
 ?>
