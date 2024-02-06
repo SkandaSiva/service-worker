42bd1e3dbd12751c05c6ff1f242365af134f7f9c
mibra004 <77853813+mibra004@users.noreply.github.com>
2024-02-05 22:50:53 -0500

    register-2

--- Login/register.php
+++ Login/register.php
@@ -21,6 +21,30 @@ $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__ . "/database.php";
 
-var_dump($password_hash);
+$sql = "INSERT INTO user (name, email, password_hash)
+        VALUES (?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",
+                  $_POST["name"],
+                  $_POST["email"],
+                  $password_hash);
+
+
+if ($stmt->execute()) {
+
+    echo "Signup Succesful";
+    
+}
+else {
+    die($mysqli->error . " " . $mysqli->errno);
+}                  
 print_r($_POST);
+var_dump($password_hash);
+
 ?>
