5d53cb32e7cfd98b6c4bfea567e0a97b85efef0f
mibra004 <mibra004@odu.edu>
2024-02-06 13:46:23 -0500

    update login

--- Login/login.php
+++ Login/login.php
@@ -1,30 +1,35 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+session_start();
 
 $is_invalid = false;
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["name"])) {
     
     $mysqli = require __DIR__ . "/database.php";
     
-    $sql = sprintf("SELECT * FROM user
-                    WHERE email = '%s'",
-                   $mysqli->real_escape_string($_POST["email"]));
+    $name = $mysqli->real_escape_string($_POST["name"]);
+    
+    $sql = sprintf("SELECT * FROM user WHERE name = '%s'", $name);
     
     $result = $mysqli->query($sql);
     
     $user = $result->fetch_assoc();
     
-    if ($user) {
+    if ($user && isset($_POST["password"])) {
         
         if (password_verify($_POST["password"], $user["password_hash"])) {
-            
-            session_start();
             
             session_regenerate_id();
             
             $_SESSION["user_id"] = $user["id"];
             
-            header("Location: index.php");
+            // Debugging statement
+            var_dump("Redirecting");
+            
+            header("Location: success-2.html");
             exit;
         }
     }
@@ -32,4 +37,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $is_invalid = true;
 }
 
+// Debugging statement
+var_dump("Not Redirecting");
+
+// Rest of your code (HTML form, error messages, etc.) goes here
 ?>
